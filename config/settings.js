0036f64490663fbd3843a22108ab0b09f8c09f53
Hox_sail <hamoudeviacom@gmail.com>
2026-06-23 16:55:58 +0300

    Add files via upload

---
+++ config/settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+    Owners: ["483507680179126272", "", ""], //الاونرات 
+    Probot: '282859044593598464',
+    port : 10850 ,
+    prefix : '?',
+    redirectUriBuy : "", // رابط الريدركت الخاص بالشراء مثل http://localhost:10850/members/add
+    لون_الامبيد : '#adc7d5',
+     channelBuyCoins : '1282309051006455828',
+
+
+
+}
