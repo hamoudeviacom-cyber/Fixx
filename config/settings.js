2e0c33ef91e3ec80f5a115f70c31bc8271345714
Hox_sail <hamoudeviacom@gmail.com>
2026-06-26 17:05:26 +0300

    Add files via upload

--- config/settings.js
+++ config/settings.js
@@ -1,12 +1,12 @@
+// =============================================
+// إعدادات المشروع - Railway-friendly
+// =============================================
 module.exports = {
-    Owners: ["483507680179126272", "", ""], //الاونرات 
+    Owners: ["483507680179126272", "", ""], //الاونرات
     Probot: '282859044593598464',
-    port : 10850 ,
-    prefix : '?',
-    redirectUriBuy : "https://fixx-production.up.railway.app/members/add", // رابط الريدركت الخاص بالشراء
-    لون_الامبيد : '#adc7d5',
-     channelBuyCoins : '1282309051006455828',
-
-
-
-}
+    port: process.env.PORT || 10850,
+    prefix: '?',
+    redirectUriBuy: process.env.REDIRECT_URI_BUY || "https://fixx-production.up.railway.app/members/add",
+    لون_الامبيد: '#adc7d5',
+    channelBuyCoins: '1282309051006455828',
+};
